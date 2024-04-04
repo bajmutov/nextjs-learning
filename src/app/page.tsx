ee66b694c35fe8aa69ca400e9e0f33900f21c6d2
Oleksandr <bajmutov@gmail.com>
2024-04-04 17:49:32 +0300

    module.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ActiveLabel from './components/active-label';
+import NotActiveLabel from './components/not-active-label';
+
 export default function Home() {
   return (
     <main>
-      <p>MAin project</p>
+      <h1>Main project Next JS</h1>
+      <ActiveLabel>Active</ActiveLabel>
+      <NotActiveLabel>Not Active</NotActiveLabel>
     </main>
   );
 }
